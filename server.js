c15ec35d7adb90098a941bba9d2584957e6e0320
Jonathan <jonathan.max.annett@gmail.com>
2021-05-30 19:24:44 +1000

    Update server.js

--- server.js
+++ server.js
@@ -481,7 +481,7 @@ function ZEDD(standalone) {
                 
                 console.log({aux});
                 console.log({user});
-                console.log({hashdPass});
+                console.log({hashedPass});
                 return aux && aux.pass2 && (aux.pass1 === user.name) && (aux.pass2.replace(base64FuglyChars,'') === hashedPass);
                 
             }
